96c5f40aa648b95e1dd10aa1573c4c0dd639a464
irmarivas <34329379+irmarivas@users.noreply.github.com>
2019-10-16 20:08:37 -0700

    update about us

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -57,22 +57,43 @@ const  About = () => {
         />
         <Divider />
         <CardMedia 
-          title="Lewis Luartz"
+          title="Winners"
           className={classes.media}
           image={profilePic}
-          alt="Lewis Luartz - Lecture"
+          alt="Winners"
         />
         <CardContent>
           <img 
             src={profilePic}
-            alt="Lewis Luartz - Lecture"
-            title="Lewis Luartz"
+            alt="Winners"
+            title="Winners"
             className={classes.inlineMedia}
           />
-          <Typography 
+          <Typography
             component="p"
             align="justify"
             variant="overline"
+          >
+            Irma and Luigi's adventure began on January 15, 2017 in Los Angeles, where they met for the first time on a blind date.
+            That first date included thai food, getting drinks at the Black Cat, watching LaLaLand at the Vista, and grabbing coffee
+            at Caffe Vita. The conversational topics were endless. The date lasted for over 6 hours, it was a date for the books. From
+            on February 12th,2017 they ran the firecracker 5k in Chinatown, where Luigi asked Irma the first of two important questions "you're 
+   
+            my girlfriend right?" To which Irma replied, "Um you haven't asked me to be your girlfriend yet." then silence, followed by 
+            laughter and Luigi proceeded to ask "Irma will you be my girlfriend?" From then on they continued to grow their journey together.
+            In mid June 2018 Luigi began to plan a special trip around Europe for the both of them. The trip was truly memorable, there were some 
+            missed flights (It's too long of a story to type), uncomfortable bed situations at hotels, but through it all being together was all that mattered and on
+            November 25, 2018 in Nice, France on the last day of the Trip, Luigi took Irma on a walk on the French Riviera where he got 
+            down on one knee and asked Irma the second of the two most important questions, "Will you marry me?" and the rest is history.
+            
+            
+            
+             
+          </Typography>
+          <Typography 
+            component="p"
+            align="justify"
+            variant="caption"
           >
             Hi there, thanks for stopping by. I'm currently under construction. 
             Luigi Campbell, Software Engineer, Full Stack Dev, Salesforce Dev. Contact: luigi.campbell@outlook.com
