e131ef0ba5a0bce977cd99d5bc10f5e0fc82316f
Luigi Campbell <luigi.campbell@outlook.com>
2019-10-26 22:49:01 -0700

    trying to set up database

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Firebase, { FirebaseContext } from './firebaseConfig'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <FirebaseContext.Provider value={Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
